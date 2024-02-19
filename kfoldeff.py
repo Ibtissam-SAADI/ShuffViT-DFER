3fc5cc7a818e3ce58e7a97a7cad74b6fe6814561
Ibtissam Saadi <118973772+Ibtissam-SAADI@users.noreply.github.com>
2024-02-19 14:30:45 +0100

    Update kfoldeff.py

--- kfoldeff.py
+++ kfoldeff.py
@@ -1,9 +1,7 @@
 import os
 for i in range(10):
-    #cmd = 'python mainlight.py --model efficientViT --bs 32 --lr 0.0002 --fold %d' %(i+1)
-    #cmd = 'python mainlight.py --model MobileVit --bs 48 --lr 0.0001 --fold %d' %(i+1)
-     cmd = 'python combinemodel.py --model Ourmodel --bs 128 --lr 0.0001 --fold %d' %(i+1)
+     cmd = 'python combinemodelkmu.py --model Ourmodel --bs 128 --lr 0.001 --fold %d' %(i+1)
      os.system(cmd)
-print("Train efficientViT ok!")
+print("Train ShuffViT ok!")
 os.system('pause')
 
